9ff489ad683409fc490c185e2e83f451be78b143
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-04-05 15:48:25 +0200

    testing

--- frontEnd/react-app/src/App.jsx
+++ frontEnd/react-app/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 // ! import Datein
-import './App.css';
+import './App.scss';
 import { HomePage } from './pages/HomePage.jsx';
 
 function App() {
--- frontEnd/react-app/src/components/InputHuman.jsx
+++ frontEnd/react-app/src/components/InputHuman.jsx
@@ -6,22 +6,27 @@
 import React, { useState } from 'react'
 import { HumanButton } from './HumanButton'
 
-export const InputHuman = ( {setRerender} ) => {
-   const [value, setValue] = useState()
+export const InputHuman = ({ setRerender }) => {
+    const [value, setValue] = useState()
 
 
-  return (
-    <section className='inputHuman'>
-        <h1> Harvest </h1>
-<h4>1 Human</h4>
-<HumanButton setRerender={setRerender} value={"1"}  ></HumanButton>
-
-<h4>5 Humans</h4>
-<HumanButton setRerender={setRerender} value={"5"}  ></HumanButton>
-
-<h4>10 Humans</h4>
-<HumanButton setRerender={setRerender} value={"10"} ></HumanButton>
-
-    </section>
-  )
+    return (
+        <section className='inputHuman'>
+            <h1> Harvest </h1>
+            <article>
+                <article>
+                    <h4>1 Human</h4>
+                    <HumanButton setRerender={setRerender} value={"1"}  ></HumanButton>
+                </article>
+                <article>
+                    <h4>5 Humans</h4>
+                    <HumanButton setRerender={setRerender} value={"5"}  ></HumanButton>
+                </article>
+                <article>
+                    <h4>10 Humans</h4>
+                    <HumanButton setRerender={setRerender} value={"10"} ></HumanButton>
+                </article>
+            </article>
+        </section>
+    )
 }
